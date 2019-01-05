bc1acb6a90762897208dc23c58dd2e9604d7fe51
Victor Fink <victor@lfpost.dk>
2019-01-05 17:23:38 +0100

    Initial commit

---
+++ Victor's PHP'ized edition/competition.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Thank You!</title>
+  <link rel="stylesheet" href="/style.css">
+</head>
+
+<body>
+<h1> Thank You for Participating in our Competition!</h1>
+<h3> Wait and find out if you won! You can either win a coffee grinder or a "Mr. Mobley Cake"!</h3>
+<h3><?php
+  /*
+  $name = $_POST['firstname'];
+  $email = $_POST['email_address'];
+  $country = $_POST['country'];
+
+  print "You participated with this information:";
+  print 'Name: ' . $name . ', email: ' . $email . ', favorite country: ' . $country;*/
+
+  echo "HELLO DUDE!";
+
+  ?>
+</body>
+</html>
---
+++ Victor's PHP'ized edition/pets.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<title>Pets</title>
+<link rel="stylesheet" href="/style.css">
+<head>
+  <br>
+
+  <h1>My pets</h1>
+</head>
+<body>
+
+<form method="get" action="Sport%20and%20food.php">
+  <div class="btn-group">
+
+    <button>Sport and food</button>
+  </div>
+
+</form>
+<p>
+<form method="get" action="index.php">
+  <div class="btn-group">
+    <button>Home</button>
+  </div>
+</form>
+
+<p>I have had a rabbit and two budgerigars, but I don't have any pets anymore.</p>
+<p>My budgerigars died a year before I moved out of Denmark, and I sold my rabbit. Now my favorite animal is a platypus.</p>
+<img src="Undulat-kanin.jpg" width="300" height="300" alt="Rabbit and budgerigar"/>
+<img src="Undulater.jpg" width="300" height="300"alt="Budgerigar"/>
+<img src="rabbitmeal.jpg" width="450" height="300" alt="Rabbit"/><br><br>
+<p></p>
+<p></p>
+<p>Please give a feedback of what you think about my site!</p>
+
+<button type="button"
+        onclick="document.getElementById('demo').innerHTML = Date()">
+  <a href="mailto:eliasbruhn123@gmail.com">Feedback!</a>
+</button>
+</body>
+</html>
